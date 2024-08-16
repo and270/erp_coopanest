6aab10ec26a181926db490c7cabb900d5f6d32c5
and270 <andresloureiross@gmail.com>
2024-08-16 16:32:08 -0300

    tela de login e registro

--- clinic_erp/settings.py
+++ clinic_erp/settings.py
@@ -113,12 +113,13 @@ AUTH_USER_MODEL = 'registration.CustomUser'
 # Internationalization
 # https://docs.djangoproject.com/en/5.0/topics/i18n/
 
-LANGUAGE_CODE = 'en-us'
+LANGUAGE_CODE = 'pt-br'
+TIME_ZONE = 'America/Sao_Paulo'
 
 TIME_ZONE = 'UTC'
 
 USE_I18N = True
-
+USE_L10N = True
 USE_TZ = True
 
 
