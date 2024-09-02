45103f50d78c6ded6a0c142be3394134a18e219b
and270 <andresloureiross@gmail.com>
2024-09-02 15:30:09 -0300

    mais ajustes de estrutura inicial

--- agenda/urls.py
+++ agenda/urls.py
@@ -2,8 +2,5 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('procedimentos/', views.agenda_procedimentos_view, name='agenda_procedimentos'),
-    path('procedimentos/add/', views.add_procedimento_view, name='add_procedimento'),
-    path('escala/', views.escala_trabalho_view, name='escala_trabalho'),
-    path('escala/add/', views.add_escala_view, name='add_escala'),
+    path('agenda/', views.agenda_view, name='agenda'),
 ]
--- agenda/views.py
+++ agenda/views.py
@@ -2,43 +2,52 @@ from django.shortcuts import render, get_object_or_404, redirect
 from .models import Procedimento, EscalaAnestesiologista
 from .forms import ProcedimentoForm, EscalaForm
 from django.contrib.auth.decorators import login_required
+from calendar import monthrange
+from datetime import datetime, timedelta
+from constants import SECRETARIA_USER, GESTOR_USER, ADMIN_USER, ANESTESISTA_USER
+
+def get_calendar_dates(year, month):
+    first_day_of_month = datetime(year, month, 1)
+    last_day_of_month = datetime(year, month, monthrange(year, month)[1])
+    
+    start_date = first_day_of_month - timedelta(days=first_day_of_month.weekday())
+
+    end_date = last_day_of_month + timedelta(days=6 - last_day_of_month.weekday())
+    
+    current_day = start_date
+    calendar_dates = []
+    
+    while current_day <= end_date:
+        calendar_dates.append({
+            'day': current_day,
+            'is_current_month': current_day.month == month
+        })
+        current_day += timedelta(days=1)
+    
+    return calendar_dates
 
 @login_required
-def agenda_procedimentos_view(request):
+def agenda_view(request):
     if not request.user.validado:
         return render(request, 'registration/usuario_nao_autenticado.html')
-    procedimentos = Procedimento.objects.all()
-    return render(request, 'agenda_procedimentos.html', {'procedimentos': procedimentos})
+    
+    today = datetime.today()
+    year = request.GET.get('year', today.year)
+    month = request.GET.get('month', today.month)
+    
+    year = int(year)
+    month = int(month)
 
-@login_required
-def add_procedimento_view(request):
-    if not request.user.validado:
-        return render(request, 'registration/usuario_nao_autenticado.html')
-    if request.method == 'POST':
-        form = ProcedimentoForm(request.POST, request.FILES)
-        if form.is_valid():
-            form.save()
-            return redirect('agenda_procedimentos')
-    else:
-        form = ProcedimentoForm()
-    return render(request, 'add_procedimento.html', {'form': form})
-
-@login_required
-def escala_trabalho_view(request):
-    if not request.user.validado:
-        return render(request, 'registration/usuario_nao_autenticado.html')
-    escalas = EscalaAnestesiologista.objects.all()
-    return render(request, 'escala_trabalho.html', {'escalas': escalas})
-
-@login_required
-def add_escala_view(request):
-    if not request.user.validado:
-        return render(request, 'registration/usuario_nao_autenticado.html')
-    if request.method == 'POST':
-        form = EscalaForm(request.POST)
-        if form.is_valid():
-            form.save()
-            return redirect('escala_trabalho')
-    else:
-        form = EscalaForm()
-    return render(request, 'add_escala.html', {'form': form})
+    calendar_dates = get_calendar_dates(year, month)
+    
+    context = {
+        'calendar_dates': calendar_dates,
+        'current_year': year,
+        'current_month': month,
+        'SECRETARIA_USER': SECRETARIA_USER,
+        'GESTOR_USER': GESTOR_USER,
+        'ADMIN_USER': ADMIN_USER,
+        'ANESTESISTA_USER': ANESTESISTA_USER,
+    }
+    
+    return render(request, 'agenda.html', context)
