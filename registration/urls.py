314ad3b9c7899b0da8760fb55a1fdf742c7f0165
and270 <andresloureiross@gmail.com>
2024-09-02 14:00:06 -0300

    first commit of develop branch to ambiente de hologação

---
+++ registration/urls.py
@@ -0,0 +1,15 @@
+from django.urls import path
+from django.contrib.auth.views import LogoutView
+from . import views
+
+urlpatterns = [
+    path('', views.home_view, name='home'),
+    path('login/', views.login_register_view, name='login_register'),
+    path('cadastro/', views.cadastro_view, name='cadastro'),
+    path('members/', views.members_view, name='members'),
+    path('delete/<str:model_name>/<int:object_id>/', views.delete_view, name='delete'),
+    path('profile/', views.profile_view, name='profile'),
+    path('edit/<str:model_name>/<int:object_id>/', views.edit_view, name='edit'),
+    path('profile/', views.profile_view, name='profile'),
+    path('logout/', LogoutView.as_view(next_page='home'), name='logout'),
+]
