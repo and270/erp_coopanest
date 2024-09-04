99290383e3a1a26ad8755fb97f9dcb335a7f592f
and270 <andresloureiross@gmail.com>
2024-09-04 14:10:46 -0300

    ajustes no modelo de procedimentos para adequar ao solicitado

--- agenda/models.py
+++ agenda/models.py
@@ -1,18 +1,36 @@
+from django.utils import timezone
 from django.db import models
-from registration.models import Anesthesiologist, Surgeon, HospitalClinic
+from constants import CIRURGIA_PROCEDIMENTO, EXAME_PROCEDIMENTO, FORA_CENTRO_PROCEDIMENTO
+from registration.models import Anesthesiologist, Surgeon, HospitalClinic, Groups
 
 class Procedimento(models.Model):
-    nome_paciente = models.CharField(max_length=255, verbose_name='Nome do Paciente')
-    procedimento = models.CharField(max_length=255, verbose_name='Procedimento')
+
+    PROCEDIMENTO_TYPE = (
+        (CIRURGIA_PROCEDIMENTO , 'Cirurgia'),
+        (FORA_CENTRO_PROCEDIMENTO , 'Procedimento Fora do Centro Cirúrgico'),
+        (EXAME_PROCEDIMENTO , 'Exame'),
+    )
+
+    group = models.ForeignKey(Groups, on_delete=models.SET_NULL, verbose_name='Grupo', null=True, blank=True)
+    procedimento_type = models.CharField(
+        max_length=40,
+        default=CIRURGIA_PROCEDIMENTO,
+        choices=PROCEDIMENTO_TYPE,
+        verbose_name='Tipo de Procedimento',
+    )
+    nome_paciente = models.CharField(max_length=255, verbose_name='Nome do Paciente', default='')
+    contato_pacinete = models.CharField(max_length=255, verbose_name='Contato do Paciente', default='')
+    procedimento = models.CharField(max_length=255, verbose_name='Procedimento', default='')
+    data_horario = models.DateTimeField(verbose_name='Data e Horário Marcados', default=timezone.now)
+    hospital = models.ForeignKey(HospitalClinic, on_delete=models.SET_NULL, null=True, blank=True, verbose_name='Hospital/Clínica')
+    outro_local = models.CharField(max_length=255, blank=True, null=True, verbose_name='Outro Local')
     cirurgiao = models.ForeignKey(Surgeon, on_delete=models.CASCADE, verbose_name='Cirurgião')
     anestesista_responsavel = models.ForeignKey(Anesthesiologist, on_delete=models.CASCADE, verbose_name='Anestesista Responsável')
-    data_horario = models.DateTimeField(verbose_name='Data e Horário Marcados')
-    hospital = models.ForeignKey(HospitalClinic, on_delete=models.CASCADE, verbose_name='Hospital/Clínica')
+    link_nps = models.URLField(blank=True, null=True, verbose_name='Link de Pesquisa de Satisfação')
     visita_pre_anestesica = models.BooleanField(default=False, verbose_name='Visita Pré-Anestésica Realizada')
-    foto_anexo = models.ImageField(upload_to='anexos/', blank=True, null=True, verbose_name='Anexo de Foto')
-    nome_responsavel_visita = models.CharField(max_length=255, blank=True, verbose_name='Nome do Responsável pela Visita')
     data_visita_pre_anestesica = models.DateField(blank=True, null=True, verbose_name='Data da Visita Pré-Anestésica')
-    link_nps = models.URLField(blank=True, verbose_name='Link de Pesquisa de Satisfação')
+    foto_anexo = models.ImageField(upload_to='anexos/', blank=True, null=True, verbose_name='Anexo Visita Pré-Anestésica')
+    nome_responsavel_visita = models.CharField(max_length=255, blank=True, verbose_name='Nome do Responsável pela Visita', default='')
     
     class Meta:
         verbose_name = "Procedimento"
@@ -22,6 +40,7 @@ class Procedimento(models.Model):
         return f'{self.procedimento} - {self.nome_paciente}'
 
 class EscalaAnestesiologista(models.Model):
+    group = models.ForeignKey(Groups, on_delete=models.SET_NULL, verbose_name='Grupo', null=True, blank=True)
     anestesiologista = models.ForeignKey(Anesthesiologist, on_delete=models.CASCADE, verbose_name='Anestesiologista')
     data_inicio = models.DateTimeField(verbose_name='Início do Turno')
     data_fim = models.DateTimeField(verbose_name='Fim do Turno')
